4cbe76418eb91bc11486ea5f3e90a0a34c0c089c
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2023-12-12 01:38:41 +0100

    feat: initial injective support

---
+++ services/injective.ts
@@ -0,0 +1,77 @@
+import {
+  WalletStrategy,
+  Wallet,
+  MsgBroadcaster,
+} from "@injectivelabs/wallet-ts";
+import { ChainSigningClient, WalletType } from "../contexts/userClients";
+import { getChainConfig } from "./chainConfig";
+import { ChainId } from "@injectivelabs/ts-types";
+import { Network } from "@injectivelabs/networks";
+import { randdropInjectiveClaimMsg } from "./contractTx";
+
+let walletStrategy: WalletStrategy;
+let msgBroadcaster: MsgBroadcaster;
+
+export const getInjectiveClients = (client: ChainSigningClient) => {
+  const chainConfig = getChainConfig(client.chain);
+
+  if (walletStrategy && msgBroadcaster) {
+    return { walletStrategy, msgBroadcaster };
+  }
+
+  walletStrategy = new WalletStrategy({
+    chainId:
+      chainConfig.chainId === "injective-888"
+        ? ChainId.Testnet
+        : ChainId.Mainnet,
+    wallet: Wallet.Keplr,
+    disabledWallets: [
+      Wallet.Cosmostation,
+      Wallet.Ledger,
+      Wallet.LedgerCosmos,
+      Wallet.LedgerLegacy,
+      Wallet.WalletConnect,
+      Wallet.Metamask,
+      Wallet.Ninji,
+      Wallet.Phantom,
+      Wallet.Torus,
+      Wallet.TrustWallet,
+      Wallet.Trezor,
+    ],
+  });
+
+  msgBroadcaster = new MsgBroadcaster({
+    walletStrategy,
+    network:
+      chainConfig.chainId === "injective-888"
+        ? Network.TestnetSentry
+        : Network.MainnetSentry,
+    simulateTx: true,
+  });
+
+  return { walletStrategy, msgBroadcaster };
+};
+
+export const signSendAndBroadcastOnInjective = async ({
+  client,
+  wallet,
+  message,
+}: {
+  client: ChainSigningClient;
+  wallet: WalletType;
+  message: {
+    wallet: string;
+    contract: string;
+    amount: string;
+    proof: string[];
+  };
+}) => {
+  const { msgBroadcaster, walletStrategy } = getInjectiveClients(client);
+
+  walletStrategy.setWallet(wallet === "keplr" ? Wallet.Keplr : Wallet.Leap);
+
+  return msgBroadcaster.broadcast({
+    injectiveAddress: message.wallet,
+    msgs: [randdropInjectiveClaimMsg(message)],
+  });
+};
