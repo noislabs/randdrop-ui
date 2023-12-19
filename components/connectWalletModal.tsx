63f6009014aff05c6c4d6e11455a503b2f2a5eb8
sontrinh16 <trinhleson2000@gmail.com>
2023-12-19 17:55:02 +0700

    minor

--- components/connectWalletModal.tsx
+++ components/connectWalletModal.tsx
@@ -206,7 +206,7 @@ export const WalletConnectModal = () => {
                           alt={`metamask_logo`}
                           unoptimized
                           object-fit="cover"
-                          width={150}
+                          fill={true}
                           className={`border border-nois-white/50 ${checked !== "metamask" && "saturdate-50 grayscale brightness-[0.3]"}`}
                         />
                       </div>
