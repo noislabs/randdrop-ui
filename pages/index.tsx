6aa315788d4f677e7746073e4605c4e4d098d750
Kais baccour <31540265+kaisbaccour@users.noreply.github.com>
2024-01-16 21:46:13 +0100

    enable-inj

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ import { ChainType, CheckResponse } from '../services/apiHelpers';
 
 // Config for live / not live randdrop chains
 export const AirdropLiveStatus: { [K in ChainType]: boolean } = {
-  "injective": false,
+  "injective": true,
   "juno": false,
   "stargaze": false,
   "aura": false,
